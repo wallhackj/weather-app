c0ad6a452974eb34970ac5d3a36ebc0895a43810
wallhack <victorhaideu@gmail.com>
2024-06-30 14:45:17 +0300

    feat:some updates

--- src/main/java/com/wallhack/weathermap/Servlets/RegisterServlet.java
+++ src/main/java/com/wallhack/weathermap/Servlets/RegisterServlet.java
@@ -9,8 +9,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 
-import java.io.IOException;
-
 import static com.wallhack.weathermap.utils.ExtraUtils.*;
 
 @WebServlet(value = "/register")
